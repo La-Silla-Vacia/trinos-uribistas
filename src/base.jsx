4452ba1576d16314585b44f2b4b8e7764b3936b6
danielmorelo <jmorelo@lasillavacia.com>
2017-06-20 17:30:19 -0500

    left crisis

--- src/base.jsx
+++ src/base.jsx
@@ -58,7 +58,7 @@ export default class Base extends Component {
 
     let content = (loading) ? (<LoadScreen />) : (
       <div className={s.inner}>
-        <p className={s.title}><strong>Instrucciones: </strong>Haga clic en los botones para saber a que coalición pertenece cada uno de los personajes</p>
+        <p className={s.title}><strong>Instrucciones: </strong>Haga clic en los botones azules de la derecha para saber a que grupo pertenece cada uno de los personajes</p>
         <Graphic personas={personas} grupos={grupos} />
       </div>
     );
